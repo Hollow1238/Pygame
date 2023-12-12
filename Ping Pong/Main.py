614d50cb9a5758f7e5674fa9ebe9d1b9858ef4cc
Hollow1238 <currietyler45@gmail.com>
2023-12-12 23:15:13 +1100

    Update Main.py
    Changes some values
    added controls and red lines to show the 'score' zone

--- Ping Pong/Main.py
+++ Ping Pong/Main.py
@@ -11,29 +11,40 @@ blue = (0,0,255)
 white = (255,255,255)
 black = (0,0,0)
 
-pos_x , pos_y = (SCREEN_x/2 , SCREEN_y/2)
-dir_x , dir_y = (5 , 0) 
+pos_x , pos_y = (30, SCREEN_y/2)
+dir_x , dir_y = (3.44 , 0)
+speed = 3.44
 
 RUNNING = True
 
 timer = pygame.time.Clock()
-framerate = 600
+framerate = 20
 
 def update():
     global pos_x
     global pos_y
     global dir_x
     global dir_y
-    if pos_x < SCREEN_x:
+    if pos_x < SCREEN_x-30:
         pos_x += dir_x
     else:
         dir_x = dir_x * -1
-        pos_x -= 1
-    if pos_x > 0:
+        pos_x -= 5
+    if pos_x > 35:
         pos_x += dir_x
     else:
         dir_x = dir_x * -1
-        pos_x += 1
+        pos_x += 5
+    if pos_y < SCREEN_y-5:
+        pos_x += dir_x
+    else:
+        dir_y = dir_y * -1
+        pos_y -= 5
+    if pos_y > 5:
+        pos_y += dir_y
+    else:
+        dir_y = dir_y * -1
+        pos_y += 5
 
 while RUNNING:
     timer.tick(framerate)
@@ -46,21 +57,23 @@ while RUNNING:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_w:
                 dir_x = 0
-                dir_y = -5
+                dir_y = -speed
             if event.key == pygame.K_a:
-                dir_x = -5
+                dir_x = -speed
                 dir_y = 0
             if event.key == pygame.K_s:
                 dir_x = 0
-                dir_y = 5
+                dir_y = speed
             if event.key == pygame.K_d:
-                dir_x = 5
+                dir_x = speed
                 dir_y = 0
     update()
 
     screen.fill(black)
 
-    pygame.draw.line(screen,white,(pos_x , pos_y),(pos_x - 25, pos_y),5)
+    pygame.draw.line(screen,white,(pos_x , pos_y),(pos_x - 5, pos_y),5)
+    pygame.draw.line(screen,red,(15 , 0),(15, SCREEN_y),30)
+    pygame.draw.line(screen,red,(1905 , 0),(1905, SCREEN_y),30)
 
     pygame.display.flip()
 
